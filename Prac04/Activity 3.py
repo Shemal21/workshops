42307cbda512e0cad6fde53ac943fe7cf9c8e2d7
jc350024 <shemalashenjude.jayawardenageekiyanage@my.jcu.edu.au>
2016-12-16 19:38:23 +1000

    First commit

---
+++ Prac04/Activity 3.py
@@ -0,0 +1,9 @@
+__author__ = 'jc350024'
+
+num = 0
+out_file = open("numbers.txt", "w")
+while num >=0:
+    num = int(input("Number:"))
+    if num >=0:
+        print(str(num), file= out_file)
+out_file.close()
---
+++ Prac04/Activity2.py
@@ -0,0 +1,10 @@
+__author__ = 'jc350024'
+
+scores = []
+score = int(input("Score: "))
+while score >= 0:
+    scores.append(score)
+    score = int(input("Score: "))
+if scores != []:
+    print("Your highest score is", max(scores))
+
